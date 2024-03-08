9e0bbf710d158356bda3dbea94d29339c85243a5
chandra-bhushan01 <chandrabhushanbgp321@gmail.com>
2024-03-08 12:42:52 +0530

    compleated BMI calc

---
+++ JavaScript/BMICalculator/script.js
@@ -0,0 +1,19 @@
+const forms = document.querySelector("form");
+const result = document.querySelector("#result");
+
+forms.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const height = parseFloat(document.querySelector("#h").value);
+  const weight = parseFloat(document.querySelector("#w").value);
+
+  const res = (weight / (height * height)).toFixed(2);
+  console.log(res);
+  if (height === "" || height < 0 || isNaN(height)) {
+    result.innerHTML = "Please give valid height!";
+  } else if (weight === "" || weight < 0 || isNaN(weight)) {
+    result.innerHTML = "Please give valid weight!";
+  } else {
+    result.innerHTML = res;
+  }
+});
