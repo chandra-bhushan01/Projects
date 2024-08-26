1d38f1fc5d6b5214a2ec430231b12e730bf7473c
chandra-bhushan01 <chandrabhushanbgp321@gmail.com>
2024-08-26 23:21:49 +0530

    context api project/discussion

--- React/react-router/src/components/About.jsx
+++ React/react-router/src/components/About.jsx
@@ -1,5 +1,5 @@
 export default function About() {
-  return (
+  return 
     <div className="py-16 bg-white">
       <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
         <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
@@ -14,13 +14,13 @@ export default function About() {
               React development is carried out by passionate developers
             </h2>
             <p className="mt-6 text-gray-600">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum
-              omnis voluptatem accusantium nemo perspiciatis delectus atque
+              Lorem ipsum dolor, sit amet consectetur adipisl̥l̥icing elit. Eum
+              omnis voluptatem accusantium nemo perspiciatis delectus atque5
               autem! Voluptatum tenetur beatae unde aperiam, repellat expedita
               consequatur! Officiis id consequatur atque doloremque!
             </p>
             <p className="mt-4 text-gray-600">
-              Nobis minus voluptatibus pariatur dignissimos libero quaerat iure
+              Nobis minus voluatibus pariatur dignissimos libero quaerat iure
               expedita at? Asperiores nemo possimus nesciunt dicta veniam
               aspernatur quam mollitia.
             </p>
@@ -28,5 +28,5 @@ export default function About() {
         </div>
       </div>
     </div>
-  );
+  ;
 }
